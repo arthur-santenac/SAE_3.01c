82fca36713c3d6e857ceaf102c0a28c2e90bb747
trojnar-paul <trojnar.paul@etu.univ-orleans.fr>
2025-10-23 11:12:14 +0200

    mise en place du flask

---
+++ monApp/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app = Flask (__name__)
+# Config options - Make sure you created a 'config.py' file.
+app.config.from_object('config')
+# To get one variable, tape app.config['MY_VARIABLE']
+from flask_bootstrap5 import Bootstrap
+Bootstrap(app)
+
