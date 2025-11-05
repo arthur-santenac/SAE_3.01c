ddc7c2f26e33b4111c98e767435bbc0bc0edd6f3
Louis-2503 <louis.leray@etu.univ-orleans.fr>
2025-11-05 12:17:15 +0100

    avancé de la page importer (il faut gérer le stockage du fichier)

--- monApp/static/script/repartition.js
+++ monApp/static/script/repartition.js
@@ -62,3 +62,66 @@
           updateCounts();
       }
   });
+
+  //Page importer
+
+const dropZone = document.getElementById("drop-zone");
+
+dropZone.addEventListener("drop", dropHandler);
+window.addEventListener("drop", (e) => {
+  if ([...e.dataTransfer.items].some((item) => item.kind === "file")) {
+    e.preventDefault();
+  }
+});
+dropZone.addEventListener("dragover", (e) => {
+  const fileItems = [...e.dataTransfer.items].filter(
+    (item) => item.kind === "file",
+  );
+  if (fileItems.length > 0) {
+    e.preventDefault();
+    if (fileItems.some((item) => item.type === "text/csv")) {
+      e.dataTransfer.dropEffect = "copy";
+    } else {
+      e.dataTransfer.dropEffect = "none";
+    }
+  }
+});
+
+window.addEventListener("dragover", (e) => {
+  const fileItems = [...e.dataTransfer.items].filter(
+    (item) => item.kind === "file",
+  );
+  if (fileItems.length > 0) {
+    e.preventDefault();
+    if (!dropZone.contains(e.target)) {
+      e.dataTransfer.dropEffect = "none";
+    }
+  }
+});
+const preview = document.getElementById("preview");
+
+function displayFiles(files) {
+  for (const file of files) {
+    if (file.type === "text/csv" || file.name.endsWith(".csv")) {
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(file.name));
+      preview.appendChild(li);
+    }
+  }
+}
+
+function dropHandler(ev) {
+  ev.preventDefault();
+  const files = [...ev.dataTransfer.items]
+    .map((item) => item.getAsFile())
+    .filter((file) => file);
+  displayFiles(files);
+}
+const fileInput = document.getElementById("file-input");
+fileInput.addEventListener("change", (e) => {
+  displayFiles(e.target.files);
+});
+const clearBtn = document.getElementById("clear-btn");
+clearBtn.addEventListener("click", () => {
+  preview.textContent = "";
+});
