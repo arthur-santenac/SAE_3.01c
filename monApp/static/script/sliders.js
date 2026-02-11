988186c64dab70a428b6b165b9d2e720deebc559
Louis-2503 <louis.leray@etu.univ-orleans.fr>
2026-02-11 09:38:02 +0100

    correction sliders

--- monApp/static/script/sliders.js
+++ monApp/static/script/sliders.js
@@ -5,121 +5,56 @@ document.addEventListener("DOMContentLoaded", () => {
   if (container) {
     const sliders = container.querySelectorAll(".slider");
     const values = container.querySelectorAll(".slider-value");
-
-    let globalTurnIndex = 0;
     
-    function updateSliders(activeIndex, newVal) {
-        newVal = Math.max(0, Math.min(TOTAL_TARGET, newVal));
-        groups[activeIndex].slider.value = newVal;
-        groups[activeIndex].number.value = newVal;
-        let currentSum = 0;
-        groups.forEach(g => currentSum += parseInt(g.slider.value));
-        let difference = currentSum - TOTAL_TARGET;
-        const otherGroups = groups.filter(g => g.index !== activeIndex);
-        let safetyCounter = 500; 
-        while (difference !== 0 && safetyCounter > 0) {
-            safetyCounter--;
-            let currentIndex = globalTurnIndex % otherGroups.length;
-            let candidate = otherGroups[currentIndex];
-            let val = parseInt(candidate.slider.value);
-
-            let changeMade = false;
-
-            if (difference > 0) {
-                if (val > 0) {
-                    val--;
-                    candidate.slider.value = val;
-                    candidate.number.value = val;
-                    difference--;
-                    changeMade = true;
-                }
-            } else {
-                if (val < TOTAL_TARGET) {
-                    val++;
-                    candidate.slider.value = val;
-                    candidate.number.value = val;
-                    difference++;
-                    changeMade = true;
-                }
-            }
-            globalTurnIndex++;
-        }
+    sliders.forEach((slider, index) => {
+            groups.push({
+                slider: slider,
+                number: values[index],
+                index: index
+            });
+        });
     }
-
-    groups.forEach((group) => {
-        group.slider.addEventListener('input', (e) => {
-            updateSliders(group.index, parseInt(e.target.value));
-        });
-        group.number.addEventListener('input', (e) => {
-            let val = parseInt(e.target.value);
-            if (isNaN(val)) val = 0;
-            updateSliders(group.index, val);
-        });
-        group.slider.addEventListener('change', (e) => {
-            updateSliders(group.index, parseInt(e.target.value));
-        });
-    });
-  }
 
   const TOTAL_TARGET = 100;
 
+  let globalTurnIndex = 0;
+
   function updateSliders(activeIndex, newVal) {
-    newVal = Math.max(0, Math.min(TOTAL_TARGET, newVal));
+      newVal = Math.max(0, Math.min(TOTAL_TARGET, newVal));
+      groups[activeIndex].slider.value = newVal;
+      groups[activeIndex].number.value = newVal;
+      let currentSum = 0;
+      groups.forEach(g => currentSum += parseInt(g.slider.value));
+      let difference = currentSum - TOTAL_TARGET;
+      const otherGroups = groups.filter(g => g.index !== activeIndex);
+      let safetyCounter = 500; 
+      while (difference !== 0 && safetyCounter > 0) {
+          safetyCounter--;
+          let currentIndex = globalTurnIndex % otherGroups.length;
+          let candidate = otherGroups[currentIndex];
+          let val = parseInt(candidate.slider.value);
 
-    groups[activeIndex].slider.value = newVal;
-    groups[activeIndex].number.value = newVal;
+          let changeMade = false;
 
-    let currentSum = 0;
-    groups.forEach((g) => (currentSum += parseInt(g.slider.value)));
-
-    let difference = currentSum - TOTAL_TARGET;
-
-    while (difference !== 0) {
-      if (difference > 0) {
-        let candidate = null;
-        let maxVal = -1;
-        groups.forEach((g, i) => {
-          if (i !== activeIndex) {
-            let val = parseInt(g.slider.value);
-            if (val > 0 && val > maxVal) {
-              maxVal = val;
-              candidate = g;
-            }
+          if (difference > 0) {
+              if (val > 0) {
+                  val--;
+                  candidate.slider.value = val;
+                  candidate.number.value = val;
+                  difference--;
+                  changeMade = true;
+              }
+          } else {
+              if (val < TOTAL_TARGET) {
+                  val++;
+                  candidate.slider.value = val;
+                  candidate.number.value = val;
+                  difference++;
+                  changeMade = true;
+              }
           }
-        });
-        if (candidate) {
-          let v = parseInt(candidate.slider.value) - 1;
-          candidate.slider.value = v;
-          candidate.number.value = v;
-          difference--;
-        } else {
-          newVal--;
-          groups[activeIndex].slider.value = newVal;
-          groups[activeIndex].number.value = newVal;
-          difference--;
-        }
-      } else if (difference < 0) {
-        let candidate = null;
-        let minVal = 101;
-        groups.forEach((g, i) => {
-          if (i !== activeIndex) {
-            let val = parseInt(g.slider.value);
-            if (val < 100 && val < minVal) {
-              minVal = val;
-              candidate = g;
-            }
-          }
-        });
-        if (candidate) {
-          let v = parseInt(candidate.slider.value) + 1;
-          candidate.slider.value = v;
-          candidate.number.value = v;
-          difference++;
-        } else {
-          difference++;
-        }
+          globalTurnIndex++;
       }
-    }
   }
 
   groups.forEach((group) => {
