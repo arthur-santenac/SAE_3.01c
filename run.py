17fe0b9a4b6c2dedb7281d8a150b8ded36ebd368
De Oliveira Hugo <o22303787@or-iut-i002-f29.iut45.univ-orleans.fr>
2026-02-12 13:01:36 +0100

    Création d'un point exe qui se lance, avec une dernière erreur a terminer car une des pages plante.

--- run.py
+++ run.py
@@ -13,20 +13,11 @@ def open_browser():
     url = "http://127.0.0.1:5000"
     print(f"--> Tentative d'ouverture automatique de : {url}")
     try:
-        # On essaie d'ouvrir le navigateur
         webbrowser.open(url)
     except Exception as e:
         print(f"Erreur lors de l'ouverture du navigateur : {e}")
 
 if __name__ == '__main__':
-    # On affiche un message CLAIR dans le terminal pour l'utilisateur
-    print("---------------------------------------------------")
-    print("APPLICATION COHORT LANCEE")
-    print("Si la page ne s'ouvre pas, cliquez ici : http://127.0.0.1:5000")
-    print("---------------------------------------------------")
 
-    # On lance le navigateur après 1.5 secondes (un peu plus long pour laisser Flask respirer)
     Timer(1.5, open_browser).start()
-    
-    # On lance le serveur
     app.run(host="127.0.0.1", port=5000, debug=False)
