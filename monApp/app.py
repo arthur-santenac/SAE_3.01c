511feea8155163ff1805a9f08e189bacb58ef7b4
De Oliveira Hugo <o22303787@or-iut-i002-f29.iut45.univ-orleans.fr>
2026-02-13 14:50:50 +0100

    Création d'un nouveau .exe qui fonctionne avec la version finale de l'application.

--- monApp/app.py
+++ monApp/app.py
@@ -1,19 +1,26 @@
 import os
-from flask import Flask, render_template, request
+import sys
+from flask import Flask
 from flask_dropzone import Dropzone
 from flask_bootstrap5 import Bootstrap
 
 app = Flask(__name__)
 
-app.config.from_object('config')
-
-
 Bootstrap(app)
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+if getattr(sys, 'frozen', False):
+    base_dir = os.path.dirname(sys.executable)
+else:
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+
+upload_path = os.path.join(base_dir, 'monApp', 'static', 'uploads')
+
+if not os.path.exists(upload_path):
+    os.makedirs(upload_path)
 
 app.config.update(
-    UPLOADED_PATH=os.path.join(os.path.dirname(__file__), "static", "uploads"),
+    SECRET_KEY='2lzUl{$*D6#`8uXqlU.', 
+    UPLOADED_PATH=upload_path,
     DROPZONE_ALLOWED_FILE_CUSTOM=True,
     DROPZONE_ALLOWED_FILE_TYPE='.csv',
     DROPZONE_MAX_FILES=1,
